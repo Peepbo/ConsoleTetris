f7925e9768718d20bb3ed1f810f31b34a72436a7
Peepbo <tngus10778@gmail.com>
2021-07-07 15:44:24 +0900

    map을 사용해 key(block char + block index)로 value(vvi)를 return받도록 구현, 회전 함수를 만들어 왼쪽이나 오른쪽을 눌렀을 때 해당 방향으로 회전하도록 구현

--- TetrisC++/TetrisC++/BlockManager.cpp
+++ TetrisC++/TetrisC++/BlockManager.cpp
@@ -1,62 +1,147 @@
 #include "BlockManager.h"
+#include "Key.h"
 
-vvi BlockManager::GetBlock(BlockKind kind)
+#include <iostream>
+
+void BlockManager::Init()
 {
-	switch (kind)
-	{
-	case BlockKind::I:
-		return { {1,1,1,1},
-				 {0,0,0,0} ,
-				 {0,0,0,0} ,
-				 {0,0,0,0} };
-		break;
-	case BlockKind::L:
-		return { {1,0,0,0},
-				 {1,0,0,0} ,
-				 {1,1,0,0} ,
-				 {0,0,0,0} };
-		break;
-	case BlockKind::J:
-		return { {0,1,0,0},
-				 {0,1,0,0} ,
-				 {1,1,0,0} ,
-				 {0,0,0,0} };
-		break;
-	case BlockKind::T:
-		return { {1,1,1,0},
-				 {0,1,0,0} ,
-				 {0,0,0,0} ,
-				 {0,0,0,0} };
-		break;
-	case BlockKind::O:
-		return { {1,1,0,0},
-				 {1,1,0,0} ,
-				 {0,0,0,0} ,
-				 {0,0,0,0} };
-		break;
-	case BlockKind::S:
-		return { {0,1,1,0},
-				 {1,1,0,0} ,
-				 {0,0,0,0} ,
-				 {0,0,0,0} };
-		break;
-	case BlockKind::Z:
-		return { {1,1,0,0},
-				 {0,1,1,0} ,
-				 {0,0,0,0} ,
-				 {0,0,0,0} };
-		break;
-	default:
-		return { {0,0,0,0},
-				 {0,0,0,0} ,
-				 {0,0,0,0} ,
-				 {0,0,0,0} };
-		break;
-	}
+	BlockMapInit();
+
+	blockIndex = 0;
+	blockKind = BlockKind::I;
 }
 
-void BlockManager::ChangeMatrix(vvi& matrix, vvi kind, Point point)
+void BlockManager::BlockMapInit()
 {
+	AddBlockMap(BlockKind::I, { { {1,0,0,0},
+						  {1,0,0,0},
+						  {1,0,0,0},
+						  {1,0,0,0} },
+
+						{ {1,1,1,1},
+						  {0,0,0,0},
+						  {0,0,0,0},
+						  {0,0,0,0} } });
+
+	AddBlockMap(BlockKind::L, { { {1,0,0,0},
+							  {1,0,0,0},
+							  {1,1,0,0},
+							  {0,0,0,0} },
+
+							{ {1,1,1,0},
+							  {1,0,0,0},
+							  {0,0,0,0},
+							  {0,0,0,0} },
+
+							{ {1,1,0,0},
+							  {0,1,0,0},
+							  {0,1,0,0},
+							  {0,0,0,0} },
+
+							{ {0,0,1,0},
+							  {1,1,1,0},
+							  {0,0,0,0},
+							  {0,0,0,0} } });
+
+	AddBlockMap(BlockKind::J, { { {0,1,0,0},
+							  {0,1,0,0},
+							  {1,1,0,0},
+							  {0,0,0,0} },
+
+							{ {1,0,0,0},
+							  {1,1,1,0},
+							  {0,0,0,0},
+							  {0,0,0,0} },
+
+							{ {1,1,0,0},
+							  {1,0,0,0},
+							  {1,0,0,0},
+							  {0,0,0,0} },
+
+							{ {1,1,1,0},
+							  {0,0,1,0},
+							  {0,0,0,0},
+							  {0,0,0,0} } });
+
+	AddBlockMap(BlockKind::T, { { {0,0,0,0},
+							  {1,1,1,0},
+							  {0,1,0,0},
+							  {0,0,0,0} },
+
+							{ {0,1,0,0},
+							  {1,1,0,0},
+							  {0,1,0,0},
+							  {0,0,0,0} },
+
+							{ {0,1,0,0},
+							  {1,1,1,0},
+							  {0,0,0,0},
+							  {0,0,0,0} },
+
+							{ {0,1,0,0},
+							  {0,1,1,0},
+							  {0,1,0,0},
+							  {0,0,0,0} } });
+
+	AddBlockMap(BlockKind::O, { { {1,1,0,0},
+							  {1,1,0,0},
+							  {0,0,0,0},
+							  {0,0,0,0} } });
+
+	AddBlockMap(BlockKind::S, { { {0,1,1,0},
+							  {1,1,0,0},
+							  {0,0,0,0},
+							  {0,0,0,0} },
+
+							{ {1,0,0,0},
+							  {1,1,0,0},
+							  {0,1,0,0},
+							  {0,0,0,0} } });
+
+	AddBlockMap(BlockKind::Z, { { {1,1,0,0},
+								  {0,1,1,0},
+								  {0,0,0,0},
+								  {0,0,0,0} },
+
+							{ {0,1,0,0},
+							  {1,1,0,0},
+							  {1,0,0,0},
+							  {0,0,0,0} } });
+}
+
+vvi BlockManager::GetBlock()
+{
+	blockIndex = 0;
+
+	std::string key;
+	key += blockChar[(int)blockKind];
+	key += (char)blockIndex;
+
+	return blockMap[key];
+}
+
+void BlockManager::AddBlockMap(BlockKind kind, std::vector<vvi> blocks)
+{
+	int blockIndex = 0;
+	for (const vvi& block : blocks)
+	{
+		std::string key;
+		key += blockChar[(int)kind];
+		key += (char)blockIndex++;
+
+		blockMap[key] = block;
+	}
+
+	maxIndexMap[kind] = blockIndex - 1;
+}
+
+void BlockManager::ChangeMatrix(vvi& matrix, const Point& point)
+{
+	std::string key;
+	key += blockChar[(int)blockKind];
+	key += (char)blockIndex;
+	vvi kind = blockMap[key];
+
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 4; j++)
@@ -64,4 +149,31 @@ void BlockManager::ChangeMatrix(vvi& matrix, vvi kind, Point point)
 			matrix[point.y + i][point.x + j] = kind[i][j];
 		}
 	}
+}
+
+void BlockManager::Rotate(const char& arrow, vvi& matrix, const Point& point)
+{
+	std::string write;
+
+	write += blockChar[(int)blockKind];
+
+	switch (arrow)
+	{
+	case LEFT:
+		if (blockIndex == 0)
+			blockIndex = maxIndexMap[blockKind];
+		else
+			blockIndex--;
+		break;
+	case RIGHT:
+		if (blockIndex == maxIndexMap[blockKind])
+			blockIndex = 0;
+		else
+			blockIndex++;
+		break;
+	}
+
+	write += (char)blockIndex;
+
+	ChangeMatrix(matrix, point);
 }
--- TetrisC++/TetrisC++/BlockManager.h
+++ TetrisC++/TetrisC++/BlockManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "defineHeader.h"
+#include <unordered_map>
 
 class BlockManager
 {
@@ -9,7 +10,21 @@ public:
 		I, L, J, T, O, S, Z
 	};
 
-	vvi GetBlock(BlockKind kind);
-	void ChangeMatrix(vvi& matrix, vvi kind, Point point);
+	std::unordered_map < std::string, vvi> blockMap;
+	std::unordered_map < BlockKind, int> maxIndexMap;
+
+	char blockChar[7]{ 'I','L','J','T','O','S','Z' };
+
+	BlockKind blockKind;
+	int blockIndex;
+
+	void Init();
+	void BlockMapInit();
+
+	void AddBlockMap(BlockKind kind, std::vector<vvi> blocks);
+
+	vvi GetBlock();
+	void ChangeMatrix(vvi& matrix, const Point& point);
+	void Rotate(const char& arrow, vvi& matrix, const Point& point);
 };
 
